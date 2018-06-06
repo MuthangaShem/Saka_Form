7d79718788f1df2c249f6393d5ce7bd90d5bdf5e
mwangi-njuguna <mwanginjuguna59@gmail.com>
2018-06-06 18:50:32 +0300

    Create test check category instance

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Category
 
-# Create your tests here.
+
+class CategoryTestClass(TestCase):
+    """
+    Initializing Category Test Class
+    """
+
+    def setUp(self):
+        self.new_category = Category(
+            category_name='Food', category_description='Everything great and sweet!')
+
+    def test_category_instance_true(self):
+        self.assertTrue(isinstance(self.new_category, Category))
