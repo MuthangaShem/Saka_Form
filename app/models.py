c1a314e57d7417670354a7bf22c5fd9b288233d0
mwangi-njuguna <mwanginjuguna59@gmail.com>
2018-06-06 18:40:50 +0300

    Create model Category

---
+++ app/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from location_field.models.plain import PlainLocationField
+
+
+class Category(models.Model):
+    """
+    Initializing Category Model
+    """
+    category_name = models.CharField(max_length=60)
+    category_description = models.TextField(null=True)
+
+    def __str__(self):
+        return self.category_name
