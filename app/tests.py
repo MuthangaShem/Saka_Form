b26b0f3050279f9d0f963a884540d969d51fdc25
mwangi-njuguna <mwanginjuguna59@gmail.com>
2018-06-06 19:16:21 +0300

    Create test update category

--- app/tests.py
+++ app/tests.py
@@ -24,3 +24,11 @@ class CategoryTestClass(TestCase):
         categories = Category.objects.all()
         self.new_category.delete_category()
         self.assertTrue(len(categories) == 0)
+
+    def test_update_category_method_true(self):
+        self.new_category.save_category()
+        self.new_category.update_category(self.new_category.id, 'Music', 'Great Music')
+        category = Category.objects.get(category_name='Music', category_description='Great Music')
+        category_updated = Category.objects.get(
+            category_name='Food', category_description='Everything great and sweet!')
+        self.assertTrue(len(category) == 1 and len(category_updated) == 0)
