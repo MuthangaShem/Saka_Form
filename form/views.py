5427629f67708e0ba37e34f7c6cd30bacd050701
thuitafaith <thuitamuthoni15@gmail.com>
2018-06-11 09:16:09 +0300

    Create templates folder

--- form/views.py
+++ form/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,HttpResponseRedirect
+from .models import Profile,Event,Category,Location
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def event_dis(request):
+    event = Event.objects.all()
+
+    return render(request, 'event.html')
