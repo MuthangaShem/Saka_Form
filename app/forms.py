2e506db0f4581fba83d7b9f5bc68ba6bfe6e4d92
muthangashem@gmail.com <MuthangaShem@gmail.com>
2018-06-13 18:48:49 +0300

    reverted

--- app/forms.py
+++ app/forms.py
@@ -10,14 +10,3 @@ class Event_Creation(forms.ModelForm):
         exclude = ('event_owner',
         			'event_created_on',
         			'event_image')
-        widgets = {
-            'event_date': DateTimePickerInput(
-            	options={
-                    "format": "MM/DD/YYYY", # moment date-time format
-                    "showClose": True,
-                    "showClear": True,
-                    "showTodayButton": True,
-                    }
-                ),
-        }
-
