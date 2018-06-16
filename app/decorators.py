7d23f2e07baf649f2b0a7248453e74310935ca3b
mwangi-njuguna <mwanginjuguna59@gmail.com>
2018-06-16 17:20:44 +0300

    Correct decorators module

--- app/decorators.py
+++ app/decorators.py
@@ -7,13 +7,14 @@ from django.contrib import messages
 def user_has_interests(function):
     def wrap(request, *args, **kwargs):
         current_user = request.user
-        profile = Profile.objects.get(profile_owner=current_user)
         if current_user.is_authenticated():
+            profile = Profile.objects.get(profile_owner=current_user)
             if not profile.profile_interest.all():
                 messages.info(request, 'Please select your interests to continue')
                 return redirect(reverse('event:interests'))
-        else:
             return function(request, *args, **kwargs)
+
+        return function(request, *args, **kwargs)
     wrap.__doc__ = function.__doc__
     wrap.__name__ = function.__name__
     return wrap
